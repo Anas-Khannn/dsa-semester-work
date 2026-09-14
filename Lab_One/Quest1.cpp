12605eaaae8f2a4a03fa5914aa4ed24dde2bd1f0
Anas Khan <130887317+Anas-Khannn@users.noreply.github.com>
2026-09-15 03:24:13 +0500

    Add solution for Quest 1

---
+++ Lab_One/Quest1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Name: Anas Khan" << endl;
+    cout << "Roll Number: 25PWCS1691" << endl;
+    cout << "Section: B" << endl;
+    cout << "Course: Data Structures" << endl;
+
+    return 0;
+}
