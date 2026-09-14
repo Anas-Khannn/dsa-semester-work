47ea6e84825f254d374bf9b679b0964f356b48f8
Anas Khan <130887317+Anas-Khannn@users.noreply.github.com>
2026-09-15 03:24:15 +0500

    Add solution for Quest 11

---
+++ Lab_One/Quest11.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int marks;
+    char grade;
+
+    cout << "Enter your marks: ";
+    cin >> marks;
+
+    grade = (marks >= 90) ? 'A' :
+            (marks >= 80) ? 'B' :
+            (marks >= 70) ? 'C' :
+            (marks >= 60) ? 'D' :
+            'F';
+
+    cout << "Grade: " << grade << endl;
+
+    return 0;
+}
